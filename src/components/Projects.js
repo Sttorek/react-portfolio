310b3d743ecb5389d7951745d856ae45457627da
Seth Torek <seth.torek@gmail.com>
2021-06-02 15:16:31 -0400

    added project 2

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,7 @@ import React from "react";
 import snoopie from "../images/snoopie.png";
 import planner from "../images/dailyplanner.png";
 import refactor from "../images/horiseon.png";
+import property from "../images/property.png";
 
 function Projects() {
   return (
@@ -13,10 +14,10 @@ function Projects() {
       </div>
       <div id="sub-projects">
         <a
-          target="blank"
-          href="https://sttorek.github.io/Workday-Scheduler-ST/"
+          target="_blank"
+          href="https://hidden-scrubland-47296.herokuapp.com/"
         >
-          <img id="project2" alt="project2" src={planner} />
+          <img id="project2" alt="project2" src={property} />
         </a>
 
         <a
@@ -24,6 +25,12 @@ function Projects() {
           href="https://sttorek.github.io/ST-Horiseon_code_refactor"
         >
           <img id="project3" alt="project3" src={refactor} />
+        </a>
+        <a
+          target="blank"
+          href="https://sttorek.github.io/Workday-Scheduler-ST/"
+        >
+          <img id="project4" alt="project4" src={planner} />
         </a>
       </div>
     </section>
